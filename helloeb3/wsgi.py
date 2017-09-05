60749968b63aa8705bc110a6fb0048e32b1ccb50
pajuhy123 <pajuhy123@gmail.com>
2017-09-05 21:29:03 +0900

    구동환경 별 settings 분기

--- helloeb3/wsgi.py
+++ helloeb3/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helloeb3.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helloeb3.settings.prod_aws_eb")
 
 application = get_wsgi_application()
